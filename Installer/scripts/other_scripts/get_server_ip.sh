8f60561d514dd28e2159279a0bda56b79fbcc71d
Peeyush Raj <praj@teramatrix.in>
2014-07-01 19:55:28 +0530

    Revert "added installer folder from nms svn"
    This reverts commit 7495728f70cc3e3ccfea87e9704c590cdbbf7978.

--- Installer/scripts/other_scripts/get_server_ip.sh
+++
@@ -1,8 +0,0 @@
-#echo `ifconfig  | grep 'eth0' | tr -s ' ' | cut -d ' ' -f5`
-
-echo `/sbin/ifconfig \
-       | grep '\<inet\>' \
-       | sed -n '1p' \
-       | tr -s ' ' \
-       | cut -d ' ' -f3 \
-       | cut -d ':' -f2`
